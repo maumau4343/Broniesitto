e1781a92215c7fef1dca29daacf5f0d9e536fc36
maumau4343 <mauricioleitesilvamaia44@gmail.com>
2025-04-25 17:19:49 -0300

    Ajustes css vercel

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 // app.js
 const express = require("express");
 const handlebars = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 const PORT = 8081;
-const path = require("path");
+
+// Caminho das views
 app.set("views", path.join(__dirname, "views"));
 
 // Middleware para receber dados de formulário e JSON
@@ -12,8 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Arquivos estáticos (CSS, imagens, JS, etc.)
-app.use(express.static("public"));
-app.use(express.static('src/public'));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Configuração do Handlebars
 app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
@@ -32,14 +33,13 @@ app.get("/calculo", (req, res) => {
   res.render("calculos");
 });
 
-app.get('/sobre', (req, res) => {
-    res.render('sobre');
+app.get("/sobre", (req, res) => {
+  res.render("sobre");
 });
-
 
 // Rota 404 para páginas não encontradas
 app.use((req, res) => {
-  res.status(404).render("404"); // Lembre de criar a view views/404.handlebars
+  res.status(404).render("404");
 });
 
 // Iniciar servidor
